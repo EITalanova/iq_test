8356e597ad2df556c427fc74559e036dc1d4e9a8
EITalanova <98708004+EITalanova@users.noreply.github.com>
2023-04-24 11:16:40 +0000

    Deploying to gh-pages from @ EITalanova/iq_test@b8be8e671c5528af2ea17e4932b37ebac63614d5 🚀

--- index.2a976f20.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".menu"),c=document.querySelector(".header__btn"),t=document.querySelector(".menu__btn-close"),n=document.querySelector(".menu__list"),l=()=>{console.log("click"),e.classList.toggle("invisible")};c.addEventListener("click",l),t.addEventListener("click",l),n.addEventListener("click",l);
-//# sourceMappingURL=index.2a976f20.js.map
--- index.a93918a5.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".menu"),c=document.querySelector(".header__btn"),n=document.querySelector(".menu__btn-close"),t=document.querySelector(".menu__list"),l=function(){console.log("click"),e.classList.toggle("invisible")};c.addEventListener("click",l),n.addEventListener("click",l),t.addEventListener("click",l)}();
-//# sourceMappingURL=index.a93918a5.js.map
